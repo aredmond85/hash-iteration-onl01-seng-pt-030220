07eb028c24e97faff4c5752fbd3040e7f6a8b32a
Austin Redmond <aredmond85@gmail.com>
2020-03-11 03:35:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,11 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  if age <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  else
+    return "No Birthday for you!"
+  end
+end
